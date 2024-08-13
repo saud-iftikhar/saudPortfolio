f64bc6969bc29bd359feb44bf2911b8b56bcc122
saudiftikhaar <saudiftikhar39@gmail.com>
2024-08-14 01:51:40 +0500

    changes

--- src/components/ProjectsSection.js
+++ src/components/ProjectsSection.js
@@ -14,19 +14,19 @@ const projects = [
    title: "React Infinite Scroll", 
    description: 
      "A scrollable bottom sheet with virtualisation support, native animations at 60 FPS and fully implemented in JS land 🔥️", 
-   getImageSrc: () => require("../images/photo2.jpg"), 
+   getImageSrc: () => require(""), 
  }, 
  { 
    title: "Photo Gallery", 
    description: 
      "A One-stop shop for photographers to share and monetize their photos, allowing them to have a second source of income", 
-   getImageSrc: () => require("../images/photo3.jpg"), 
+   getImageSrc: () => require(""), 
  }, 
  { 
    title: "Event planner", 
    description: 
      "A mobile application for leisure seekers to discover unique events and activities in their city with a few taps", 
-   getImageSrc: () => require("../images/photo4.jpg"), 
+   getImageSrc: () => require(""), 
  }, 
 ]; 
  
